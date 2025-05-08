266b3c60ec2dd4fdf2a6e29ad451ca892672382d
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-05-08 16:19:07 -0600

    add pgotos to python list data

--- server/app.py
+++ server/app.py
@@ -10,21 +10,24 @@ projects = [
         "title": "Mixtape Maker",
         "description": "Create and share playlists.",
         "url": "https://github.com/bijorgo/mixtape-maker",
-        "demo": "https://mixtape-maker.netlify.app"
+        "demo": "https://mixtape-maker.netlify.app",
+        "photo": "https://i.imgur.com/lsDKqa6.jpeg"
     },
     {
         "id": 1,
         "title": "Thyme to Grow",
         "description": "A virtual gardening sim with a Flask API and Pygame frontend.",
         "url": "https://github.com/bijorgo/thyme-to-grow",
-        "demo": "https://youtu.be/demo-link"
+        "demo": "https://youtu.be/demo-link",
+        "photo": "https://i.imgur.com/lsDKqa6.jpeg"
     },
     {
         "id": 3,
         "title": "Photo Gallery",
         "description": "A photo gallery featuring a React frontend",
         "url": "https://github.com/bijorgo/photo-gallery",
-        "demo": "https://youtu.be/demo-link"
+        "demo": "https://youtu.be/demo-link",
+        "photo": "https://i.imgur.com/lsDKqa6.jpeg"
     }
 ]
 
