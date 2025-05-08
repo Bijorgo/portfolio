f355dc63968fd2e100ad55c2cb6947ce4c11ba97
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-05-08 16:29:58 -0600

    add alt text to python list data

--- server/app.py
+++ server/app.py
@@ -11,7 +11,8 @@ projects = [
         "description": "Create and share playlists.",
         "url": "https://github.com/bijorgo/mixtape-maker",
         "demo": "https://mixtape-maker.netlify.app",
-        "photo": "https://i.imgur.com/lsDKqa6.jpeg"
+        "photo": "https://i.imgur.com/lsDKqa6.jpeg",
+        "alt": "Temp Alt Text"
     },
     {
         "id": 1,
@@ -19,7 +20,8 @@ projects = [
         "description": "A virtual gardening sim with a Flask API and Pygame frontend.",
         "url": "https://github.com/bijorgo/thyme-to-grow",
         "demo": "https://youtu.be/demo-link",
-        "photo": "https://i.imgur.com/lsDKqa6.jpeg"
+        "photo": "https://i.imgur.com/lsDKqa6.jpeg",
+        "alt": "Temp Alt Text"
     },
     {
         "id": 3,
@@ -27,7 +29,8 @@ projects = [
         "description": "A photo gallery featuring a React frontend",
         "url": "https://github.com/bijorgo/photo-gallery",
         "demo": "https://youtu.be/demo-link",
-        "photo": "https://i.imgur.com/lsDKqa6.jpeg"
+        "photo": "https://i.imgur.com/lsDKqa6.jpeg",
+        "alt": "Temp Alt Text"
     }
 ]
 
